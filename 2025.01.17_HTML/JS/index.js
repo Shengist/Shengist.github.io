74a0be55c3c554acbc8a5bbc8dff70f010399251
Hsu Sheng-Chun <30403346+Shengist@users.noreply.github.com>
2025-01-19 21:06:06 +0800

    20250119 增加小樹屋

---
+++ 2025.01.17_HTML/JS/index.js
@@ -0,0 +1,39 @@
+const swiper = new Swiper(".poster ", {
+  autoplay: true,
+  loop: true,
+  pagination: {
+    el: ".dots-list",
+    bulletClass: "bullet",
+    bulletActiveClass: "bullet-active",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".next-btn",
+    prevEl: ".pre-btn",
+  },
+});
+const promoteCarousel = new Swiper(".promote-carousel", {
+  autoplay: true,
+  loop: true,
+  slidesPerView: 2,
+  spaceBetween: 20,
+  speed: 5000,
+  freeMode: true,
+  autoplay: {
+    delay: 0,
+    disableOnInteraction: false,
+    pauseOnMouseEnter: true,
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+    992: {
+      slidesPerView: 5,
+    },
+    1280: {
+      slidesPerView: 5,
+    },
+  },
+});
